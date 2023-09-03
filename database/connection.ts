af0b423df318867485b4695886cbf7a6f3879fd8
Théo <tduval@student.42.fr>
2023-09-03 20:58:16 +0200

    removed mongodb

--- database/connection.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const main = async () => {
-  mongoose.connect(
-    "mongodb+srv://speudal:HFTGcplqdCjhHN2N@cluster55064.tqjcutk.mongodb.net/?retryWrites=true&w=majority"
-  );
-};
-
-export default main;
