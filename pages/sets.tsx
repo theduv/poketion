84cb4349c6b21186a69e1865ae600996b643bf12
Théo <tduval@student.42.fr>
2023-09-03 21:20:03 +0200

    you can now search for a set

--- pages/sets.tsx
+++ pages/sets.tsx
@@ -3,6 +3,8 @@ import SetPreview from "../components/sets/setPreview";
 
 const Sets = () => {
   const [sets, setSets] = useState([]);
+  const [displayedSets, setDisplayedSets] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const fetchSets = async () => {
     try {
@@ -10,21 +12,45 @@ const Sets = () => {
       if (res) {
         const body = await res.json();
         setSets(body.data);
+        setDisplayedSets(body.data);
       }
     } catch (e) {
       console.log(e);
     }
   };
 
+  const onChangeSearch = (e: any) => {
+    setSearchValue(e.target.value);
+  };
+  useEffect(() => {
+    setDisplayedSets(
+      sets.filter(
+        (set: any) =>
+          set.id.toLowerCase().includes(searchValue.toLowerCase()) ||
+          set.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    );
+  }, [searchValue]);
+
   useEffect(() => {
     fetchSets();
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-x-2 gap-y-2">
-      {sets.map((set) => (
-        <SetPreview set={set} />
-      ))}
+    <div className="flex flex-col space-y-2">
+      <input
+        value={searchValue}
+        onChange={onChangeSearch}
+        type="text"
+        placeholder="Search by set name or id"
+        className="bg-gray-700 text-white rounded-lg px-2 py-2 w-1/4"
+      />
+
+      <div className="grid grid-cols-3 gap-x-2 gap-y-2">
+        {displayedSets.map((set) => (
+          <SetPreview set={set} />
+        ))}
+      </div>
     </div>
   );
 };
